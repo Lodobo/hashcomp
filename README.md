ab5cf2b70a14273f52d31b107f8b49e69d1fa620
lodobo <lodobo303@gmail.com>
2023-11-28 21:28:08 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# hashcomp
+
+A simple command line tool to check whether some files have the same sha256 checksum.
+
+## Usage:
+
+hashcomp.py [-h] FILE [...]
---
+++ hashcomp.py
@@ -0,0 +1,24 @@
+import sys, argparse, hashlib
+
+def main():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("files", metavar="FILE", nargs="+", help="Path to file")
+    args = parser.parse_args()
+
+    checksums = list()
+    
+    for file in args.files:
+        with open(file, "rb") as f: # "rb" for binary   
+            hexdigest = hashlib.file_digest(f, "sha256").hexdigest()
+            checksums.append(hexdigest)
+            print(hexdigest, file)
+    
+    if len(set(checksums)) == 1:
+        print("All checksums are identical")
+    else:
+        print("The checksums are different")
+
+if __name__ == "__main__":
+    main(parse_arguments())
+   
