d3d56e9069ffe4b900d6452be032526a87c83077
lodobo <lodobo303@gmail.com>
2023-11-28 22:07:52 +0100

    Update hashcomp.py: changed list to set

--- hashcomp.py
+++ hashcomp.py
@@ -6,15 +6,15 @@ def main():
     parser.add_argument("files", metavar="FILE", nargs="+", help="Path to file")
     args = parser.parse_args()
 
-    checksums = list()
+    checksums = set()
     
     for file in args.files:
         with open(file, "rb") as f: # "rb" for binary   
             hexdigest = hashlib.file_digest(f, "sha256").hexdigest()
-            checksums.append(hexdigest)
+            checksums.add(hexdigest)
             print(hexdigest, file)
     
-    if len(set(checksums)) == 1:
+    if len(checksums) == 1:
         print("All checksums are identical")
     else:
         print("The checksums are different")
